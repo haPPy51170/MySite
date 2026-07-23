04e928c045dec7ddca589b0e8dd8be82a7dfb646
MohammadAli SHAD <mohammadalishad241@gmail.com>
2026-07-23 15:23:21 +0430

    mplement login page, template inheritance and simplify custom cursor

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('about/', views.about_view, name='about'),
     path('login/', views.login_view, name='login'),
-    path('web/', views.web_view, name='web'),
+    path('home/', views.home_view, name='home'),
 ]
 
--- website/views.py
+++ website/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def home_view(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
+def home_view(request):
+    return render(request,'index.html')
 def about_view(request):
     return render(request,'about.html')
 
 def login_view(request):
     return render(request,'login.html')
 
-def web_view(request):
-    return render(request,'index.html')
 # Create your views here.
